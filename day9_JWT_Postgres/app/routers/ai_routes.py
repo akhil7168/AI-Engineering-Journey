92af91e551cfb9c3bdf37fb5d4d29a0e0fd0048f
akhil7168/MinorProject <akhilpunyala@gmail.com>
2026-07-15 16:51:44 +0530

    Day 33 - Conversation Memory using Redis

--- day9_JWT_Postgres/app/routers/ai_routes.py
+++ day9_JWT_Postgres/app/routers/ai_routes.py
@@ -1,8 +1,16 @@
 from fastapi import APIRouter
 
-from schemas import ChatRequest
+from schemas import (
+    ConversationRequest,
+    ConversationResponse
+)
 
 from app.services.ai_service import generate_ai_response
+
+from app.services.memory_service import (
+    get_conversation,
+    clear_conversation
+)
 
 router = APIRouter(
     prefix="/ai",
@@ -12,17 +20,52 @@ router = APIRouter(
 
 @router.post(
     "/chat",
-    summary="Chat with Local AI",
-    description="Interact with the local Ollama model using different AI personalities."
+    response_model=ConversationResponse,
+    summary="Chat with AI",
+    description="Chat with AI using conversation memory."
 )
-async def chat(request: ChatRequest):
+async def chat(
+    request: ConversationRequest
+):
 
     response = generate_ai_response(
+        session_id=request.session_id,
         prompt=request.prompt,
         mode=request.mode
     )
 
+    return ConversationResponse(
+        session_id=request.session_id,
+        response=response
+    )
+
+
+@router.get(
+    "/history/{session_id}",
+    summary="Get Conversation History"
+)
+def get_history(
+    session_id: str
+):
+
+    conversation = get_conversation(session_id)
+
     return {
-        "mode": request.mode,
-        "response": response
+        "session_id": session_id,
+        "messages": conversation
+    }
+
+
+@router.delete(
+    "/history/{session_id}",
+    summary="Clear Conversation"
+)
+def delete_history(
+    session_id: str
+):
+
+    clear_conversation(session_id)
+
+    return {
+        "message": "Conversation cleared successfully."
     }
